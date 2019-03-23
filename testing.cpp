b51ce9d87c2a2b9daa8d1ba823859f964e941253
Poletayson <dmitriipoletaev_1997@mail.ru>
2019-03-23 17:18:40 +0700

    Создан тест test_WidthScaling ()

--- testing.cpp
+++ testing.cpp
@@ -2,11 +2,16 @@
 
 Testing::Testing()
 {
-
+    obj = new ImageConverter();
 }
 
 void Testing::test_ClassCreate ()     //тест на создание класса
 {
     ImageConverter* ptr = new ImageConverter();
     QVERIFY(ptr != Q_NULLPTR);
+}
+
+void Testing::test_WidthScaling ()  //тестирование масштабирования по ширине
+{
+    QCOMPARE(obj->WidthScaling ("1.jpg"), QSize (1280, 1280));
 }
