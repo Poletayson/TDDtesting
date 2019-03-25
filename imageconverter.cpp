6e86c30a8a2c0511002aeb0e589eabb95100f4f4
Poletayson <dmitriipoletaev_1997@mail.ru>
2019-03-25 20:12:56 +0700

    Модифицирован метод для прохождения test_Save()

--- imageconverter.cpp
+++ imageconverter.cpp
@@ -21,17 +21,12 @@ QString ImageConverter::Open (QString n)
 QString ImageConverter::Save (QImage* im, QString f)
 {
     //todo
-//    if (!im->isNull())
-//    {
-//        im->save(f);
-//        QStringList ptrL = f.split(".");
-//        return ptrL.last();
-//    }
-
-//    if (f == "jpg")
-//        return f;
-//    else
-//        return "png";
+    if (!im->isNull())
+    {
+        im->save(f);
+        QStringList ptrL = f.split(".");
+        return ptrL.last();
+    }
 }
 
 QSize ImageConverter::WidthScaling (QString n)  //приведение высоты к ширине
